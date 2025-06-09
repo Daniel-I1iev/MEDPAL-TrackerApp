d8e781591dbf95197212e3f7355224f344a9dc5c
Daniel-I1iev <d.iliev293945@abv.bg>
2025-06-09 21:59:51 +0300

    Add files via upload

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'medtrack-patient-pal-main',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'medtrack-patient-pal-main',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
